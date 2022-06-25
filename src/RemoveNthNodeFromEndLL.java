1de10b8c99af7a5f3d2d7c59633229fc4032ff3d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-24 22:13:05 -0700

    added comments for future references, any how I stil dont know why did we perform count -- in the beginning fo the else statement

--- src/RemoveNthNodeFromEndLL.java
+++ src/RemoveNthNodeFromEndLL.java
@@ -9,24 +9,31 @@ public class RemoveNthNodeFromEndLL {
         }
     }
     public static Node removeNthNodeFromEndLL(Node head, int n){
+        //take two nodes and initialize the size
         Node current = head;
         Node previous = head;
         int size = 0;
+        //until current becomes 0
         while(current != null){
             size ++;
             current = current.next;
         }
+        //after getting the size get the count
         int count = size -n;
+        //if its 0 means, we are asked to delete the first node, so we just return the next node
         if(count ==0){
             return head.next;
         }
+        //else we decrement the node
         else{
             count --;
+            //and run a loop to reach the exact point where we want disconnection
             while(count >0){
                 count --;
                 previous = previous.next;
             }previous.next = previous.next.next;
             return head;
+            //and tada, head is returned
         }
     }
 
