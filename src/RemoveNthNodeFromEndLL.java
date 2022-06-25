564398864f37476942ef44390beaa537c03593c2
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-24 22:08:44 -0700

    addeda  RemoveNthNodeFromEndLL method

--- src/RemoveNthNodeFromEndLL.java
+++ src/RemoveNthNodeFromEndLL.java
@@ -8,6 +8,27 @@ public class RemoveNthNodeFromEndLL {
             this.next = null;
         }
     }
+    public static Node removeNthNodeFromEndLL(Node head, int n){
+        Node current = head;
+        Node previous = head;
+        int size = 0;
+        while(current != null){
+            size ++;
+            current = current.next;
+        }
+        int count = size -n;
+        if(count ==0){
+            return head.next;
+        }
+        else{
+            count --;
+            while(count >0){
+                count --;
+                previous = previous.next;
+            }previous.next = previous.next.next;
+            return head;
+        }
+    }
 
     public static void main(String[] args) {
 
